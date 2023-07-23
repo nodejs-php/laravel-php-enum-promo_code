b74ef53abd281a1fb329f3fc30a0fde2d3cf8719
aleksey <av.pavlow@gmail.com>
2023-07-23 19:39:54 +0300

    Добавляет подсказки для навигации по моделям

---
+++ _ide_helper_models.php
@@ -0,0 +1,100 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property float $total_price
+ * @property float $discount_price
+ * @property float $final_price
+ * @property int|null $promocode_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\OrderFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereDiscountPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereFinalPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePromocodeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereTotalPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Promocode
+ *
+ * @property int $id
+ * @property string $code
+ * @property float $discount
+ * @property \App\Enums\PromocodeTypes $type
+ * @property bool $active
+ * @property $condition
+ * @property \Illuminate\Support\Carbon|null $expires_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PromocodeFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereCondition($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereDiscount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promocode whereUpdatedAt($value)
+ */
+	class Promocode extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
